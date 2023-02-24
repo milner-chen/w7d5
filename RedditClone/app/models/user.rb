688524e7a57a2eb2cfe96af424c27756297e078d
Milner Chen <milnerchen0@gmail.com>
2023-02-24 11:07:35 -0500

    create password=

---
+++ RedditClone/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+# figvaperb
+# figvap
+
+class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        bcrypt = BCrypt::Password.new(self.password_digest)
+        bcrypt.is_password?(password)
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+        self.save!
+    end
+
+end
